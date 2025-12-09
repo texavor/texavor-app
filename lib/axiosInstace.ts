5a5213ece66db3ef5d4f5fec9b59374ca14a1516
Suraj Vishwakarma <surajvishwakarma625@gmail.com>
2025-12-09 13:39:56 +0530

    easywrite -> texavor 01

--- lib/axiosInstace.ts
+++ lib/axiosInstace.ts
@@ -5,7 +5,7 @@ import * as Sentry from "@sentry/nextjs";
 export const baseURL =
   process.env.NODE_ENV == "development"
     ? "http://localhost:3000"
-    : "https://api.texavor.com";
+    : "https://www.api.texavor.com";
 
 // export const baseURL = "https://api.easywrite.dev";
 
